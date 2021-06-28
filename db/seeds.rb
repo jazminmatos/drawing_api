9b10f101619574a6256cd98bc1240bc5e136987f
jazminmatos <jmatos1993@gmail.com>
2021-06-27 20:13:51 -0400

    commit tree f21f3810caada6415a2618d3f8b3faf1c589bdbe

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create([
+    {username: "Samantha", id: 1},
+    {username: "Danny", id: 2},
+    {username: "Benjamin", id: 3}
+
+])
+
+Drawing.create([
+    {id: 1, image: , user_id: 1}
+    {id: 2, image: , user_id: 1}
+    {id: 3, image: , user_id: 2}
+    {id: 4, image: , user_id: 2}
+    {id: 5, image: , user_id: 3}
+    {id: 6, image: , user_id: 3}
+    {id: 7, image: , user_id: 3}
+])
