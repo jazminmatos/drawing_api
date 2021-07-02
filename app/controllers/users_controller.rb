b4caa2d0f2b523d2ce429d885c40189bd16f608e
jazminmatos <jmatos1993@gmail.com>
2021-07-02 17:22:41 -0400

    rendered related drawing data in JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
-      render json: user, status: :created, location: user
+      render json: user, 
+      include: [:drawings],
+      status: :created, location: user
     else
       render json: user.errors, status: :unprocessable_entity
     end
@@ -46,6 +48,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username)
+      params.require(:user).permit(:username, :drawings_ids => [], :drawings => [])
     end
 end
