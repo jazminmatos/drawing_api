62f7006169e53ff03f1c06353a24642dc8b97b7a
jazminmatos <jmatos1993@gmail.com>
2021-07-02 17:09:38 -0400

    converted @drawing to drawing

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -3,45 +3,45 @@ class DrawingsController < ApplicationController
 
   # GET /drawings
   def index
-    @drawings = Drawing.all
+    drawings = Drawing.all
 
-    render json: @drawings
+    render json: drawings
   end
 
   # GET /drawings/1
   def show
-    render json: @drawing
+    render json: drawing
   end
 
   # POST /drawings
   def create
-    @drawing = Drawing.new(drawing_params)
+    drawing = Drawing.new(drawing_params)
 
-    if @drawing.save
-      render json: @drawing, status: :created, location: @drawing
+    if drawing.save
+      render json: drawing, status: :created, location: drawing
     else
-      render json: @drawing.errors, status: :unprocessable_entity
+      render json: drawing.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /drawings/1
   def update
-    if @drawing.update(drawing_params)
-      render json: @drawing
+    if drawing.update(drawing_params)
+      render json: drawing
     else
-      render json: @drawing.errors, status: :unprocessable_entity
+      render json: drawing.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /drawings/1
   def destroy
-    @drawing.destroy
+    drawing.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_drawing
-      @drawing = Drawing.find(params[:id])
+      drawing = Drawing.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
